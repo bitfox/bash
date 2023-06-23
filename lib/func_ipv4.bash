1ee794e471fe7962de764b8effdeb0775c2a048f
Oliver <133523047+bitfox@users.noreply.github.com>
2023-06-23 22:04:51 +0200

    Add files via upload

--- lib/func_ipv4.bash
+++ lib/func_ipv4.bash
@@ -6,13 +6,63 @@
 #
 #
 # 2023-06-23 13:18 CEST	Oliver Lenz	Initial
+# 2023-06-23 21:18 CEST	Oliver Lenz	Neue Funkionen angehangen
+# 2023-06-23 22:02 CEST	Oliver Lenz	Zusammenführen von ctype_ipv4 und func_ipv4
 #
 #
 
+
+
 #
-# Bibliothek ctype_ipv4 gesucht...
+# Handelt es sich um eine numerische CIDR?
 #
-. /home/mobaxterm/ctype_ipv4.bash
+#
+function ctype_ipv4_cidr(){
+	[[ "${1}" == 0 || ${1} =~ ^([0-9]+)$ && ${BASH_REMATCH[1]:0:1} != "0" && ${BASH_REMATCH[1]} -le 32 ]] && true || false
+}
+
+#
+# Handelt es sich um eine valide IPv4-Addresse?
+#
+#
+function ctype_ipv4_address(){
+	[[
+		${1} =~ ^([0-9]+)\.([0-9]+)\.([0-9]+)\.([0-9]+)$  && \
+		( ${BASH_REMATCH[1]} == "0" || ${BASH_REMATCH[1]:0:1} != "0" ) && ${BASH_REMATCH[1]} -le 255 &&  \
+		( ${BASH_REMATCH[2]} == "0" || ${BASH_REMATCH[2]:0:1} != "0" ) && ${BASH_REMATCH[2]} -le 255 &&  \
+		( ${BASH_REMATCH[3]} == "0" || ${BASH_REMATCH[3]:0:1} != "0" ) && ${BASH_REMATCH[3]} -le 255 &&  \
+		( ${BASH_REMATCH[4]} == "0" || ${BASH_REMATCH[4]:0:1} != "0" ) && ${BASH_REMATCH[4]} -le 255 
+	]] && true || false
+}
+
+#
+# Handelt es sich um eine valide IPv4-Netzmaske?
+# Beispiel: 255.128.0.0
+#
+# Achtung: 
+# IPv4 kann auch "verdrehte" Netzmasken, wie z.B. 255.0.255.0
+# Wir Prüfen hier aber auf linksbündige ununterbrochene Masken.
+#
+function ctype_ipv4_netmask(){
+	if [[ ${1} =~ ^([0-9]+)\.([0-9]+)\.([0-9]+)\.([0-9]+)$ ]]; then 
+		local s="" i=0 k=0 
+		for((i=1;i<=4;i++)); do
+			for((k=7;k>=0;k--)); do
+				if [[ ${BASH_REMATCH[$i]} == "0" || ${BASH_REMATCH[$i]:0:1} != "0" && ${BASH_REMATCH[$i]} -le 255 ]]; then
+					(( $((2**${k})) & ${BASH_REMATCH[${i}]} )) && s+="1" || s+="0"
+				else
+					false && return
+				fi
+			done
+		done
+		i=0 
+		while [ "${s:0:1}" == "1" ]; do (( i+=1 )); s=${s:1}; done
+		while [ "${s:0:1}" == "0" ]; do (( i+=1 )); s=${s:1}; done
+		[ $i -eq 32 ] && true && return
+	fi
+	false
+}
+
 
 
 #
@@ -112,7 +162,7 @@ function func_ipv4_form_cidr(){
 
 
 #
-# Egal von Netmask oder CIDR - immer CIDR ausgeben
+# Egal ob  Netmask oder CIDR - immer CIDR ausgeben
 # 255.0.0.0 => 255.0.0.0
 # 8         => 255.0.0.0
 #
