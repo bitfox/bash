d523ce23d290f32a7a44acb2ee99a56c31546a65
Oliver <133523047+bitfox@users.noreply.github.com>
2025-10-08 12:49:27 +0200

    Update func_orcharhino.bash

--- lib/func_orcharhino.bash
+++ lib/func_orcharhino.bash
@@ -1,5 +1,7 @@
 #funktionen rund um die Paketverwaltung etc.
 
+
+
 function get_uptime(){
   local zeitzone="${1}"
   [ "${zeitzone}" != "" ] && zeitzone="Europe/Berlin"
@@ -9,10 +11,27 @@ function get_uptime(){
 function last_rpmupdate(){
    local zeitzone="${1}"
    [ "${zeitzone}" == "" ] && zeitzone="Europe/Berlin"
-   while IFS= read datum; do
-     env TZ="${zeitzone}" date -d "${datum}" "+%Y-%m-%d %H:%M:%S %Z"
-   done< <( rpm -qa --last 2>/dev/null | sed -E 's/^[^ ]+//g' | sed -E 's/^[ ]+(.*)/\1/g' | sort | uniq | tail -1 - )
+   while IFS= read zeitstempel; do
+     env TZ="${zeitzone}" date -d "${zeitstempel}" "+%Y-%m-%d %H:%M:%S %Z"
+   done< <( rpm -qa --last 2>/dev/null | sed -E 's/^[^ ]+//g' | sed -E 's/^[ ]+(.*)/\1/g' | sort -n | uniq | tail -1 - )
 }
+
+function last_aptupdate(){
+  local zeitzone="${1}"
+  [ "${zeitzone}" == "" ] && zeitzone="Europe/Berlin"
+  zeitstempel="$(stat -c '%z' /var/cache/apt/pkgcache.bin 2>/dev/null)"
+  [ "${zeitstempel}" != "" ] && env TZ="${zeitzone}" date -d "${zeitstempel}" "+%Y-%m-%d %H:%M:%S %Z"
+}
+
+function last_packageupdate(){
+  local zeitzone="${1}"
+  [ "${zeitzone}" == "" ] && zeitzone="Europe/Berlin"
+  (
+    last_aptupdate "${zeitzone}"
+    last_rpmupdate "${zeitzone}"
+  ) | grep -E "^[0-9]{4}" | sort -n | uniq | tail -1 -
+}
+
 
 function foreman_hammer_get_proxies(){ 
   hammer --output csv --no-headers proxies list --fields="name" 
