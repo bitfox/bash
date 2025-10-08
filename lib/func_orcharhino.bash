e18bf3f751f7f953d5ff70aa598279fa6ed55ba8
Oliver <133523047+bitfox@users.noreply.github.com>
2025-10-08 12:01:53 +0200

    Update func_orcharhino.bash

--- lib/func_orcharhino.bash
+++ lib/func_orcharhino.bash
@@ -2,7 +2,7 @@
 
 function last_rpmupdate(){
   local timezone="${1}"
-  [ "${timezone} == "" ] && timezone="Europe/Berlin"
+  [ "${timezone}" == "" ] && timezone="Europe/Berlin"
   while IFS= read datum; do
     env TZ="${timezone}" date -d "${datum}" "+%Y-%m-%d %H:%M:%S %Z"
   done< <( rpm -qa --last 2>/dev/null | sed -E 's/^[^ ]+//g' | sed -E 's/^[ ]+(.*)/\1/g' | uniq ) | sort | uniq | tail -1 -
