748bab2313b513d03ad1c79f2bb1cc7417c891d0
Oliver <133523047+bitfox@users.noreply.github.com>
2025-10-08 12:23:32 +0200

    Update func_orcharhino.bash

--- lib/func_orcharhino.bash
+++ lib/func_orcharhino.bash
@@ -1,11 +1,17 @@
-#funktionen rund um die paketverwaltung
+#funktionen rund um die Paketverwaltung etc.
+
+function get_uptime(){
+  local zeitzone="${1}"
+  [ "${zeitzone}" != "" ] && zeitzone="Europe/Berlin"
+  env TZ="${zeitzone}" date -d "$(env TZ="${zeitzone}" uptime -s)" "+%Y-%m-%d %H:%M:%S %Z"
+}
 
 function last_rpmupdate(){
-  local timezone="${1}"
-  [ "${timezone}" == "" ] && timezone="Europe/Berlin"
-  while IFS= read datum; do
-    env TZ="${timezone}" date -d "${datum}" "+%Y-%m-%d %H:%M:%S %Z"
-  done< <( rpm -qa --last 2>/dev/null | sed -E 's/^[^ ]+//g' | sed -E 's/^[ ]+(.*)/\1/g' | uniq ) | sort | uniq | tail -1 -
+   local zeitzone="${1}"
+   [ "${zeitzone}" == "" ] && zeitzone="Europe/Berlin"
+   while IFS= read datum; do
+     env TZ="${zeitzone}" date -d "${datum}" "+%Y-%m-%d %H:%M:%S %Z"
+   done< <( rpm -qa --last 2>/dev/null | sed -E 's/^[^ ]+//g' | sed -E 's/^[ ]+(.*)/\1/g' | sort | uniq | tail -1 - )
 }
 
 function foreman_hammer_get_proxies(){ 
