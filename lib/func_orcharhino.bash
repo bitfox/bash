b8bd1012ef66f197d492d600e0906599d470bc10
Oliver <133523047+bitfox@users.noreply.github.com>
2025-10-08 11:54:52 +0200

    Create func_orcharhino.bash

---
+++ lib/func_orcharhino.bash
@@ -0,0 +1,13 @@
+#funktionen rund um die paketverwaltung
+
+function last_rpmupdate(){
+  local timezone="${1}"
+  [ "${timezone} == "" ] && timezone="Europe/Berlin"
+  while IFS= read datum; do
+    env TZ="${timezone}" date -d "${datum}" "+%Y-%m-%d %H:%M:%S %Z";
+  done< <( rpm -qa --last 2>/dev/null | sed -E 's/^[^ ]+//g' | sed -E 's/^[ ]+(.*)/\1/g' | uniq ) | sort | uniq | tail -1 -
+}
+
+function foreman_get_proxies(){ 
+  hammer --output csv --no-headers proxies list --fields="name" 
+}
